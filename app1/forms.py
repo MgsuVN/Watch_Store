5fa2fcfaff297b0cc363d34962adfa1f615d435b
Ngọc Tiến Trần <tien11102005@gmail.com>
2026-02-05 21:24:26 +0700

    new update

--- app1/forms.py
+++ app1/forms.py
@@ -4,4 +4,4 @@ from .models import Watch
 class WatchForm(forms.ModelForm):
     class Meta:
         model = Watch
-        fields = ['name', 'price', 'description', 'image']
+        fields = ['name', 'price', 'description', 'image', 'brand']
